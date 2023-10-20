50374797064a771c1b693f00d0f13f43cef0fc65
Ishita <ishita1001@gmail.com>
2023-10-20 15:06:13 -0400

    Add Category Filter

---
+++ expense_tracker/src/model/CategoryFilter.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CategoryFilter implements TransactionFilter{
+    private String category;
+
+    public CategoryFilter(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public List<Transaction> filter(List<Transaction> transactions) {
+
+        System.out.println("Total transac before filtering: " + transactions.size());
+
+        List<Transaction> filteredTransactions = transactions.stream()
+                .filter(transaction -> transaction.getCategory().equalsIgnoreCase(category))
+                .collect(Collectors.toList());
+
+
+        return filteredTransactions;
+    }
+}
